9ea56284ea0e18e1a3a3a39e1e0b519b795d6df6
vlecommandeur <valentin.lecommandeur@gmail.com>
2022-12-16 15:38:39 +0000

    chore: improve wasm bindings

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,9 @@ mod random;
 use minesweeper::*;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+#[wasm_bindgen(js_name = getState)]
+pub fn get_state() -> String {
+    let ms = Minesweeper::new(10, 10, 5);
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    ms.to_string()
 }
