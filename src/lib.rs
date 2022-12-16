7fcf1a388fbd6711dc58b513326a10be6ab017fe
vlecommandeur <valentin.lecommandeur@gmail.com>
2022-12-16 16:35:00 +0000

    feat: improve ui and wasm bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 mod minesweeper;
 mod random;
 
+use std::cell::RefCell;
+
 use minesweeper::*;
 use wasm_bindgen::prelude::*;
 
+thread_local! {
+    static MINESWEEPER: RefCell<Minesweeper> = RefCell::new(Minesweeper::new(10, 10, 5));
+}
+
 #[wasm_bindgen(js_name = getState)]
 pub fn get_state() -> String {
-    let ms = Minesweeper::new(10, 10, 5);
+    MINESWEEPER.with(|ms| ms.borrow().to_string())
+}
 
-    ms.to_string()
+#[wasm_bindgen(js_name = openField)]
+pub fn open_field(x: usize, y: usize) {
+    MINESWEEPER.with(|ms| {
+        ms.borrow_mut().open((x, y));
+    })
 }
